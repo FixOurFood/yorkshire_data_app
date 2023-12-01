b4ed628a7634cd1b0c33891db4817cba6d58d817
jucordero <obejaamarilla@hotmail.com>
2023-12-01 12:37:16 +0000

    comparison plot and dynamic dataset notes

--- yorkshire_data_app.py
+++ yorkshire_data_app.py
@@ -1,11 +1,32 @@
 import streamlit as st
-import geopandas as gpd
-
+import matplotlib.pyplot as plt
 from streamlit_folium import st_folium
 
 from map_config import maps_yorkshire, cloropleth_folium_yorkshire
 from gui_config import *
 from data_config import data, metadata
+
+def long_str_trunc(label, leng=45):
+    ''' Shortens a long string to 45 characters or less.
+    If longer than 45 characters, it adds an elipsis at the end.
+
+    Parameters
+    ----------
+    label : str
+        String to shorten
+    leng : int
+        Maximum number of characters before shortening and appending an elipsis.
+
+    Returns
+    -------
+    str
+        Shortened label.
+    '''
+
+    if len(label) > leng:
+        return label[:leng] + "..."
+    else:
+        return label 
 
 # Reduce the padding at the top of the page. 1rem results in some clipping
 st.set_page_config(layout="wide")
@@ -27,14 +48,17 @@ main_cols = st.columns((2,4,2))
 with main_cols[0]:
 
     option_main_category = st.selectbox("Main category",
-                                        list(data.keys())[2:])
+                                        list(data.keys())[2:],
+                                        format_func=long_str_trunc)
     
     # Fetch information from metadata
     info_row = metadata[metadata["column"] == option_main_category]
 
     st.markdown("## " + option_main_category)
-    st.markdown('### Description')
-    st.markdown(info_row["notes"].values[0])
+
+    if isinstance(info_row["notes"].values[0], str):
+        st.markdown('### Description')
+        st.markdown(info_row["notes"].values[0])
     st.markdown('### Sources')
     st.markdown(info_row["source_name"].values[0])
     st.markdown(info_row["url"].values[0])     
@@ -59,5 +83,17 @@ with main_cols[1]:
                      hide_index=True,
                      height=710)
         
+    elif option == "Comparison":
+        compare_main_category = st.selectbox("Compare against",
+                                        list(data.keys())[2:],
+                                        format_func=long_str_trunc)
+        
+        f, ax = plt.subplots()
+        ax.plot(data[option_main_category], data[compare_main_category], 'o')
+        ax.set_xlabel(option_main_category)
+        ax.set_ylabel(compare_main_category)
+
+        st.pyplot(f)
+               
 with main_cols[2]:
     pass
